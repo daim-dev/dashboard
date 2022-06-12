a65a9a6dcdf74096db9e906d6335c6244c1553fd
Damien Robinson <damien.robinson@daim.dev>
2022-06-12 19:02:55 +1000

    refactor: add home page

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <Html lang="en-AU" itemscope itemtype="https://schema.org/WebPage">
+    <div class="min-h-screen flex flex-col justify-between body">
+      <main
+        itemprop="mainContentOfPage"
+        itemtype="https://schema.org/WebPageElement"
+      >
+        <NuxtPage />
+      </main>
+    </div>
+  </Html>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+<Title v-if="title" itemprop="name">{{ title }}</Title>
+    <Meta
+      v-if="description"
+      name="description"
+      :content="description"
+      itemprop="description"
+    />
+  <div>
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Daim Dashboard',
+      description: 'Daim Dashboard'
+    }
+  }
+}
+</script>
