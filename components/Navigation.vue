1de4acb704732868d7cad39a5a70191826053bd4
Damien Robinson <damien.robinson@daim.dev>
2022-06-22 08:48:17 +1000

    fix(navigation): move sidebar under nav

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,12 +1,22 @@
 <template>
   <nav
-    class="relative w-full flex flex-wrap items-center justify-between text-lg"
+    class="relative w-full flex flex-wrap items-center justify-between text-lg shadow-md"
     bg-nav
   >
     <div
-      class="container-fluid w-full flex flex-wrap items-center justify-between px-6 h-16"
+      class="container-fluid w-full flex flex-wrap items-center justify-between px-3 h-64px"
     >
-      <div class="container-fluid">
+      <div class="container-fluid flex">
+        <button
+          class="navbar-toggler flex text-white border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
+          type="button"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+          @click="draw = !draw"
+        >
+          <span class="i-mdi-menu h-24px w-24px my-auto"></span>
+        </button>
         <nuxt-link
           class="flex items-center text-black-900 hover:text-black-900 focus:text-black-900 mt-2 lg:mt-0 mr-1"
           to="/"
@@ -21,53 +31,28 @@
         </nuxt-link>
       </div>
 
-      <div class="hidden lg:flex justify-between">
+      <div class="flex justify-between">
         <Link
           v-bind="item"
           v-for="item of items"
           :key="item.name"
-          class="text-xl hover:text-gray-200 btn font-bold text-white-500"
+          class="text-xl hover:text-gray-200 btn text-white-500"
         />
       </div>
 
       <teleport to="#sidenav">
         <div
-          class="w-60 h-full shadow-md bg-nav px-1 absolute top-0 left-0 flex-col lg:hidden"
-          :class="{ hidden: !draw, flex: draw }"
+          class="flex h-full shadow-md bg-white px-1 flex-col transition-all duration-500"
+          :class="{ 'opacity-0': !draw, 'w-0': !draw, 'w-60': draw }"
         >
           <Link
             v-bind="item"
             v-for="item of items"
             :key="item.name"
-            class="relative text-xl hover:text-grey-500 btn font-bold text-white-500"
+            class="relative text-xl hover:text-grey-500 btn text-black-500"
           />
         </div>
       </teleport>
-
-      <button
-        class="navbar-toggler lg:hidden text-black border-0 hover:shadow-none hover:no-underline py-2 px-2.5 bg-transparent focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline"
-        type="button"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-        @click="draw = !draw"
-      >
-        <svg
-          aria-hidden="true"
-          focusable="false"
-          data-prefix="fas"
-          data-icon="bars"
-          class="w-6"
-          role="img"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-        >
-          <path
-            fill="currentColor"
-            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-          ></path>
-        </svg>
-      </button>
     </div>
   </nav>
 </template>
