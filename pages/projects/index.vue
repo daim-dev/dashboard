f5834b478b7a8880c50b4c53f4ef089789fcd943
Damien Robinson <damien.robinson@daim.dev>
2022-06-22 22:17:49 +1000

    feat(projects): add cards

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -7,18 +7,41 @@
       :content="description"
       itemprop="description"
     />
-    <h1 class="text-3xl">{{title}}</h1>
+    <h1 class="text-3xl">{{ title }}</h1>
+    <button class="btn btn-primary" @click="create">Add new Project</button>
+    <div>
+      <Card v-for="item of items" :key="item.name" v-bind="item"></Card>
+    </div>
   </div>
 </template>
 
-
 <script>
+import Card1 from "../../components/Card.vue";
 export default {
-  data() {
-    return {
-      title: 'Projects',
-      description: 'Projects'
-    }
-  }
-}
+    data() {
+        return {
+            title: "Projects",
+            description: "Projects",
+            items: [
+                {
+                    description: "Eudits is an auditing platform that enables technical users to complete complex audits performed by people.",
+                    image: "https://daim.dev/img/portfolio/eudits.png",
+                    links: [
+                        {
+                            url: "https://www.eudits.com/",
+                            name: "Website",
+                        },
+                    ],
+                    name: "Eudits",
+                    startDate: "2022-05-25T05:57:04Z",
+                },
+            ],
+        };
+    },
+    methods: {
+        create() {
+            console.log("create");
+        },
+    },
+};
 </script>
