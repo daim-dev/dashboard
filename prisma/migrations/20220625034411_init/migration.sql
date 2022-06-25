4a22453f2c5e159514a5d280d4602b513bb554de
Damien Robinson <damien.robinson@daim.dev>
2022-06-25 13:53:39 +1000

    fix(schema): rename tables to plural

--- prisma/migrations/20220625034411_init/migration.sql
+++ prisma/migrations/20220625034411_init/migration.sql
@@ -1,16 +1,16 @@
 -- CreateTable
-CREATE TABLE "Organization" (
+CREATE TABLE "organizations" (
     "id" UUID NOT NULL DEFAULT extensions.uuid_generate_v1(),
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Organization_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "organizations_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Project" (
+CREATE TABLE "projects" (
     "id" UUID NOT NULL DEFAULT extensions.uuid_generate_v1(),
     "name" TEXT NOT NULL,
     "description" TEXT NOT NULL,
@@ -18,19 +18,19 @@ CREATE TABLE "Project" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "organizationId" UUID NOT NULL,
 
-    CONSTRAINT "Project_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Person" (
+CREATE TABLE "persons" (
     "id" UUID NOT NULL DEFAULT extensions.uuid_generate_v1(),
     "givenName" TEXT NOT NULL,
     "familyName" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Person_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "persons_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "Project" ADD CONSTRAINT "Project_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "Organization"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "projects" ADD CONSTRAINT "projects_organizationId_fkey" FOREIGN KEY ("organizationId") REFERENCES "organizations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
