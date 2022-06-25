17e61cbfb718c6a7998badc75c5b0bd5c3290f78
Damien Robinson <damien.robinson@daim.dev>
2022-06-25 14:56:16 +1000

    fix(btn style): remove rounded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,7 +34,7 @@ export default defineNuxtConfig({
     },
     shortcuts: [
       {
-        btn: 'py-2 px-4 rounded-lg px-6 py-2.5',
+        btn: 'py-2 px-4 px-6 py-2.5',
         'btn-link':
           'bg-transparent',
         'form-control': 'block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
