c37d9e1f987f06f761a52ed0590a4c6ff42ef6fd
Damien Robinson <damien.robinson@daim.dev>
2022-06-26 19:31:24 +1000

    fix(styles): add form label shortcut

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,6 +39,7 @@ export default defineNuxtConfig({
           'bg-transparent',
         'form-control': 'block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none',
         'form-group': 'mb-6',
+        'form-label': 'form-label inline-block mb-2 text-gray-700',
       },
       // dynamic shortcuts
       [
